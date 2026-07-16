bafd034d4ca606f196399e524989cad854b6b654
George makulu <wamakologeorge@gmail.com>
2026-07-16 07:27:28 -0500

    Export Autonomous ReAct engine

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,5 +1,12 @@
 """One cognitive core for Amosclaud Autonomous."""
 
 from .actions import AutonomousOrchestrator, AutonomousTask, run_autonomous
+from .react_loop import AutonomousReactLoop, ReactOutcome
 
-__all__ = ["AutonomousOrchestrator", "AutonomousTask", "run_autonomous"]
+__all__ = [
+    "AutonomousOrchestrator",
+    "AutonomousReactLoop",
+    "AutonomousTask",
+    "ReactOutcome",
+    "run_autonomous",
+]
