b27d96e7eebe44a6a159df5850001e4953dabaff
George makulu <wamakologeorge@gmail.com>
2026-07-19 14:08:35 -0500

    Avoid circular import in repository package

--- repository/__init__.py
+++ repository/__init__.py
@@ -1,6 +1,5 @@
 """Native Amosclaud repository storage and service connector."""
 
 from .connector import RepositoryConnector, RepositoryConnectorError, RepositoryRecord
-from .git_server import router
 
-__all__ = ["RepositoryConnector", "RepositoryConnectorError", "RepositoryRecord", "router"]
+__all__ = ["RepositoryConnector", "RepositoryConnectorError", "RepositoryRecord"]
