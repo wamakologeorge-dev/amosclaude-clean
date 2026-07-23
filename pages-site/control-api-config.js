517db1a242f6e1869e6d8d617c31f8f513b17364
George makulu <wamakologeorge@gmail.com>
2026-07-23 13:30:08 -0500

    Configure website control API endpoint

---
+++ pages-site/control-api-config.js
@@ -0,0 +1,7 @@
+(() => {
+  "use strict";
+
+  // Public configuration only. No token, key, or credential belongs in this file.
+  // The backend authenticates users with an HttpOnly secure session cookie.
+  window.AMOSCLAUD_CONTROL_API = "https://www.amosclaud.com";
+})();
