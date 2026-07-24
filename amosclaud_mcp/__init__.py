8a27a2b0a324def445fec816276037df9c5ede55
George makulu <wamakologeorge@gmail.com>
2026-07-24 11:19:13 -0500

    Add Amosclaud MCP package

---
+++ amosclaud_mcp/__init__.py
@@ -0,0 +1,5 @@
+"""Amosclaud Model Context Protocol integration."""
+
+from amosclaud_mcp.client import AmosclaudClient, AmosclaudClientConfig, AmosclaudMCPError
+
+__all__ = ["AmosclaudClient", "AmosclaudClientConfig", "AmosclaudMCPError"]
