470fa21dfe2ca58cf93d45cae7f7d69c4a9c2fd3
George makulu <wamakologeorge@gmail.com>
2026-07-26 17:13:36 -0500

    feat: start isolated workspace worker service

---
+++ workspace_worker/__init__.py
@@ -0,0 +1,5 @@
+"""Private Amosclaud workspace-worker service.
+
+This package is deployed separately from the public web application. It is the
+only service that may communicate with the workspace container daemon.
+"""
