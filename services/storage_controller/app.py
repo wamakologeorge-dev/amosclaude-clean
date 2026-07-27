7fde1c4f2d44a515698c38de1b95ec66b8d40dca
George makulu <wamakologeorge@gmail.com>
2026-07-27 14:30:47 -0500

    Include the managed large-volume mount root in controller defaults

--- services/storage_controller/app.py
+++ services/storage_controller/app.py
@@ -1,7 +1,14 @@
 """Storage-controller application composition root."""
 
-from controller import app
-from volume_api import router as volume_router
+import os
+
+os.environ.setdefault(
+    "AMOSCLAUD_STORAGE_ALLOWED_MOUNTS",
+    "/var/lib/amosclaud/repositories,/data/repositories,/mnt/amosclaud-volumes",
+)
+
+from controller import app  # noqa: E402
+from volume_api import router as volume_router  # noqa: E402
 
 app.include_router(volume_router)
 
