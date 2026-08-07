dc9a15cd1de286ee791854c66020972aa1fdb90a
George makulu <wamakologeorge@gmail.com>
2026-08-07 19:34:12 +0200

    Fix Black formatting in MCP feature flag test

--- tests/test_plugin_mcp_feature_flags.py
+++ tests/test_plugin_mcp_feature_flags.py
@@ -8,7 +8,6 @@ from amoscloud_ai.api.routes import auth
 from amoscloud_ai.extensions.runtime import get_registry
 from amoscloud_ai.main import create_app
 
-
 ROOT = Path(__file__).resolve().parents[1]
 
 
