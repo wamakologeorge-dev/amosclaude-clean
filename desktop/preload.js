683b60b3b24a9b9254030e18cf06599d0310132c
George makulu <wamakologeorge@gmail.com>
2026-08-26 21:23:24 -0500

    feat: configure Amosclaud Desktop gateway provider

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,6 +1,14 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('amosclaudDesktop', {
   platform: process.platform,
   isDesktopApp: true,
+  openProviderSetup: () => ipcRenderer.invoke('desktop:open-provider-setup'),
+  provider: {
+    getConfig: () => ipcRenderer.invoke('provider:get'),
+    saveConfig: (config) => ipcRenderer.invoke('provider:save', config),
+    clearConfig: () => ipcRenderer.invoke('provider:clear'),
+    testConnection: () => ipcRenderer.invoke('provider:test'),
+    request: (request) => ipcRenderer.invoke('provider:request', request),
+  },
 });
