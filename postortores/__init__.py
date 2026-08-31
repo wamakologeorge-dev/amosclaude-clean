bad61dd4b86a319889c5a91f110cd944cd5a2ede
George makulu <wamakologeorge@gmail.com>
2026-08-31 17:16:30 -0500

    feat(postortores): export artifact and snapshot services

--- postortores/__init__.py
+++ postortores/__init__.py
@@ -1,12 +1,16 @@
 """Amosclaud Postortores native data-system package."""
 
+from .artifacts import ArtifactStore
 from .engine import PostortoresEngine
 from .service import PostortoresService
+from .snapshot import SnapshotManager
 from .types import DataRecord, EvidenceRecord, EventRecord, MemoryRecord
 
 __all__ = [
+    "ArtifactStore",
     "PostortoresEngine",
     "PostortoresService",
+    "SnapshotManager",
     "DataRecord",
     "EvidenceRecord",
     "EventRecord",
