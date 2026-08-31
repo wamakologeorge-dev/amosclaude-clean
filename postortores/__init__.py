c3d7e8abd88813a3b7b7ede30ca2fc33a5c2ea91
George makulu <wamakologeorge@gmail.com>
2026-08-31 16:58:45 -0500

    feat(postortores): export native service contract

--- postortores/__init__.py
+++ postortores/__init__.py
@@ -1,10 +1,12 @@
 """Amosclaud Postortores native data-system package."""
 
 from .engine import PostortoresEngine
+from .service import PostortoresService
 from .types import DataRecord, EvidenceRecord, EventRecord, MemoryRecord
 
 __all__ = [
     "PostortoresEngine",
+    "PostortoresService",
     "DataRecord",
     "EvidenceRecord",
     "EventRecord",
